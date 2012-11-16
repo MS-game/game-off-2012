97aab93e17a890fd4a47e966ec4b6e7df43dddfe
Sietse <sietse@Sietse-Ubuntu.(none)>
2012-11-16 16:19:20 +0100

    Added a loop

--- src/msgame/Main.java
+++ src/msgame/Main.java
@@ -1,9 +1,61 @@
 package msgame;
 
 public class Main {
+	/** Ticks per second */
+	public static final int TPS = 60;
+	/** Frames per second */
+	public static final int FPS = 60;
+	
 	public Main ()
 	{
+		initDisplay();
+		initScene();
+		startLoop();
+		clean();
+	}
+	public void initDisplay ()
+	{
+	}
+	public void initScene ()
+	{
+	}
+	public void startLoop ()
+	{
+		long lastTime = System.currentTimeMillis();
+		double unprocessed = 0;
 		
+		for (;;)
+		{
+			long now = System.currentTimeMillis();
+			long delta = (now - lastTime);
+			lastTime = now;
+			unprocessed += delta / (1000.0 / TPS);
+			
+			boolean ticked = false;
+			while (unprocessed > 1)
+			{
+				ticked = true;
+				tick();
+				unprocessed--;
+			}
+			if (ticked) render();
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	public void render ()
+	{
+		
+	}
+	public void tick ()
+	{
+		
+	}
+	public void clean ()
+	{
 	}
 	public static void main (String[] args)
 	{
