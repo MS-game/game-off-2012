672d75700e323abd60243fdd8a7bd186252d9e23
Sietse <sietse97@gmail.com>
2012-11-17 16:58:41 +0100

    Added gravity to boxes.

--- src/msgame/EntityBox.java
+++ src/msgame/EntityBox.java
@@ -10,12 +10,20 @@ public class EntityBox extends Entity {
         height = width = 10;
         updateAABB();
     }
-    public void tick () {
-        if (world.thePlayer.aabb.intersects(aabb) && aabb.solveY(1, world.thePlayer.aabb) > 0) {
-            move(((world.thePlayer.x > x) ? -1 : 1), 0);
-        }
+
+    public void tick() {
+        yspeed += 0.05;
+        if (world.thePlayer.aabb.intersects(aabb)
+                && aabb.solveY(1, world.thePlayer.aabb) > 0)
+            xspeed = (world.thePlayer.x > x) ? -1 : 1;
+        else
+            xspeed = 0;
+        move();
+        if (onground)
+            yspeed = 0;
     }
-    public void render (Graphics g) {
+
+    public void render(Graphics g) {
         g.drawImage(world.spriteholder.getSprite(3), (int) x, (int) y,
                 (int) (x + 10), (int) (y + 10), 0, 0, 10, 10, null);
     }
