db12dce5b46f63e99ebb73f79a9e804b9ed9bd7b
Sietse <sietse97@gmail.com>
2012-11-19 16:33:45 +0100

    Added font class.

---
+++ src/msgame/Fonts.java
@@ -0,0 +1,8 @@
+package msgame;
+
+import java.awt.Font;
+
+public class Fonts {
+    public static final Font fpsCounter = new Font("Dialog", Font.PLAIN, 12);
+    public static final Font clickToFocus = new Font("Dialog", Font.PLAIN, 12);
+}
